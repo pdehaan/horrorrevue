8f3c496f4aa56d90324d83fdca510fb16c61a594
Charity Gamble <charlibydesign@gmail.com>
2022-10-11 15:36:53 -0600

    added more files to project

---
+++ .eleventy.js
@@ -0,0 +1,44 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('static')
+    eleventyConfig.addPassthroughCopy('admin')
+    eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
+  
+    const striptags = require("striptags");
+
+    const {
+      DateTime
+    } = require("luxon");
+  
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+      eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {
+          zone: 'utc'
+        }).toFormat('yy-MM-dd');
+      });
+  
+      eleventyConfig.addFilter("readableDate", dateObj => {
+      return DateTime.fromJSDate(dateObj, {
+        zone: 'utc'
+      }).toFormat("MM-dd-yy");
+    });
+
+    function extractExcerpt(article) {
+        if (!article.hasOwnProperty("templateContent")) {
+          console.warn(
+            'Failed to extract excerpt: Document has no property "templateContent".'
+          );
+          return null;
+        }
+      
+        let excerpt = null;
+        const content = article.templateContent;
+      
+        excerpt = striptags(content)
+          .substring(0, 300) // Cap at 200 characters
+          .replace(/^\s+|\s+$|\s+(?=\s)/g, "")
+          .trim()
+          .concat("...");
+        return excerpt;
+      }
+  };
+  
